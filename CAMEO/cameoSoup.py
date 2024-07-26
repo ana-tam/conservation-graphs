b4de537b2b723cbc457f29bca2ec572ced9fd020
ana-tam <96597077+ana-tam@users.noreply.github.com>
2024-07-26 19:56:49 +0100

    Update WikiContentSoup.py

--- CAMEO/cameoSoup.py
+++ CAMEO/cameoSoup.py
@@ -1,16 +1,15 @@
 #The following code makes calls via a list of URLs compiled into a .csv file.  
-#It iterates through each URL in the .csv and extracts the h1, h2's and related p's (immediatey following the h2's) from each page.
-#The intended use for this code in this project is to transform html content from the Cameo materials database wiki into .json.
+#It iterates through each URL in the .csv and extracts only the h1, h2's and related p's (immediatey following the h2's) from each page.
+#The intended use for this code in this project is to transform html content from a Wiki-based materials database into .json.
 
 import json
-    import csv
-    import requests
-    from bs4 import BeautifulSoup
-    #url='http://cameo.mfa.org/wiki/Acetylene'  #Uncomment this line if passing in URL directly and comment out line 10, datafile variable.
-    results = open("CameoMaterialsDB.json", 'a', newline='')
-    datafile = open("Cameo_URLs.csv", 'r', newline='')
+import requests
+from bs4 import BeautifulSoup
+#url='http://URL_HERE'  #Uncomment this line if passing in URL directly and comment out line 10, datafile variable.
+results = open("Results.json", 'a', newline='')
+datafile = open("Wiki_Content.csv", 'r', newline='')
     
-    urlReader = datafile.read().splitlines() #NB:  without this step, it doesn't open the csv properly and iterates through with bad/useless/empty results, until last row.
+urlReader = datafile.read().splitlines() #NB:  without this step, it doesn't open the csv properly and iterates through with bad/useless/empty results, until last row.
     
     for url in urlReader:
          req = requests.get(url)
@@ -33,16 +32,17 @@ import json
              subtext = y.get_text()
              listSubText.append(subtext)
     
-         zippy = (zip(listHeads2, listSubText))
-         c = [dict(zippy)]
+         zipH2group = (zip(listHeads2, listSubText))
+         dictH2group = [dict(zipH2group)]
     
-         zappy = (zip(listHeads1, c))
-         b = [dict(zappy)]
+         zipH1TopGroup = (zip(listHeads1, dictH2group))
+         dictH1TopGroup = [dict(zipH1TopGroup)]
     
-         json.dump(b, results)
+         json.dump(dictH1TopGroup, results)
     results.close()
+
     # Uncomment any one of the following 'print' commands to test run.  otherwise can ignore all prints to follow
     #print(listHeads2)
     #print(listSubText)
-    #print(c)
-    #print(b)
+    #print(dictH2group)
+    #print(dictH1TopGroup)
